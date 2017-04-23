5712f3023b99476ed475f1b66342e9edf136ba31
kbgb666 <kbgb666@gmail.com>
2017-04-23 21:03:51 +0300

    example test

---
+++ app/tests/test_web.py
@@ -0,0 +1,13 @@
+from app.views import login, validateUser
+import unittest
+
+
+class name(unittest.TestCase):
+
+    def test(self):
+        invalid_login = self.app.post('login', data=dict(login='invalid', password='invalid'), follow_redirects=True)
+        assert 'Invalid credentials' in invalid_login.data
+
+
+if __name__ == '__main__':
+    unittest.main()

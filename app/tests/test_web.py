acba556f726fe9f0cc16416fcde26ead7af51ffb
kbgb666 <kbgb666@gmail.com>
2017-04-24 11:55:24 +0300

    first test

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -2,13 +2,24 @@ from app.views import login, validateUser
 import unittest
 import nose
 import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object('flask_config')
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+from app import views, models
 
 
 class name(unittest.TestCase):
 
-    def test(self):
-        invalid_login = self.app.post('login', data=dict(login='invalid', password='invalid'), follow_redirects=True)
-        assert 'Invalid credentials' in invalid_login.data
+    def test_p(self):
+        self.assertFalse(validateUser("rom","siksik","22"))
         #testttgsdf
 
 

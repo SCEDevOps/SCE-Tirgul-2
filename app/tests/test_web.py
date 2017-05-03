755a29911fb93a5fe13fd69946eb1a6c67637412
kbgb666 <kbgb666@gmail.com>
2017-05-03 11:50:09 +0300

    first test

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -6,21 +6,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object('flask_config')
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
 
-from app import views, models
+
+from app import views, models, app
 
 
 class name(unittest.TestCase):
 
+    def create_app(self):
+        app = Flask(__name__)
+        app.config.from_object('flask_config')
+        db = SQLAlchemy(app)
+        login_manager = LoginManager()
+        login_manager.init_app(app)
+        login_manager.login_view = 'login'
+
     def test_p(self):
         self.assertFalse(validateUser("rom","siksik","22"))
         #testttgsdf
+
+    def test_d(self):
+        self.app =app.test_client()
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        #TEST ADDED
+
+    # def test_invaild_login(self):
+    #     self.app = app.test_client()
+    #     invalid_login = app.post('login', data=dict(login='invalid', password='invalid'), follow_redirects=True)
+    #     assert 'Invalid credentials' in invalid_login.data
 
 
 if __name__ == '__main__':

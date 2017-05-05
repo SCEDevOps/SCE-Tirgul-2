ad1d723d53dfab9b1284cb2a697e977d1da2cf64
LITALHO <litalhoresh@gmail.com>
2017-05-05 15:48:47 +0300

    Second integration test- Try to Get In with incorrect details

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -1,8 +1,12 @@
+from threading import Thread
 
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 ################# Get In with correct details #################
+from selenium.webdriver.support.wait import WebDriverWait
+
 chromedriver = "/Users/LITALHO/PycharmProjects/SCE-Tirgul-2/chromedriver"
 browser = webdriver.Chrome(chromedriver)
 browser.get('http://127.0.0.1:5000/')
@@ -15,3 +19,21 @@ id_Input.send_keys("66")
 id_Input.send_keys(Keys.ENTER)
 browser.close()
 #################################################################
+
+
+################# Try to Get In with incorrect details ##########
+
+chromedriver = "/Users/LITALHO/PycharmProjects/SCE-Tirgul-2/chromedriver"
+browser = webdriver.Chrome(chromedriver)
+browser.get('http://127.0.0.1:5000/')
+first_name_Input = browser.find_element_by_id("first_name")
+first_name_Input.send_keys("lilo")
+last_name_Input = browser.find_element_by_id("last_name")
+last_name_Input.send_keys("horesh")
+id_Input =  browser.find_element_by_id("user_id")
+id_Input.send_keys("66")
+id_Input.send_keys(Keys.ENTER)
+WebDriverWait(browser, 3)
+browser.close()
+#################################################################
+

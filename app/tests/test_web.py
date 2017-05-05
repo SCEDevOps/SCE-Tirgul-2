b2f8fd4dee3c23cdc91b81b92ab9efd59f0f2d2b
LITALHO <litalhoresh@gmail.com>
2017-05-05 18:24:16 +0300

    second unit test- Cant log in without id

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -8,13 +8,19 @@ class AppTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = app
-        self.tester = self.app.test_client(self)
-        self.tester.testing = True
+        self.check = self.app.test_client(self)
+        self.check.testing = True
 
 ######## The manager page is not Unaccessible from the url ########
     def test_Manager_Option(self):
-        response = self.tester.get('/app/manager')
+        response = self.check.get('/app/manager')
         self.assertEqual(response.status_code, 404) #404=not founf
+###################################################################
+
+##################### Cant log in without id ######################
+    def test_missing_id_(self):
+        response = self.check.post('login',data=dict(first_name='lilo', last_name='siksik'))
+        self.assertEqual(response.status_code, 400) # 400=Bad Request
 ###################################################################
 
 if __name__ == '__main__':

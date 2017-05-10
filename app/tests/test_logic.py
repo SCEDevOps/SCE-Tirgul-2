4411d728672fac21efa48be775a578a26207774a
LITALHO <litalhoresh@gmail.com>
2017-05-10 11:39:01 +0300

    selenium tests fixed

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -5,11 +5,13 @@ from app import app
 
 class AppTestCae(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
     def test_Manager_Option(self):
         #../tests/chromedriver
         ################# Get In with correct details #################
-        browser = webdriver.Chrome()
+        browser =  self.browser
         browser.get('http://127.0.0.1:5000/')
         first_name_Input = browser.find_element_by_id("first_name")
         first_name_Input.send_keys("lilo")
@@ -18,12 +20,12 @@ class AppTestCae(unittest.TestCase):
         id_Input = browser.find_element_by_id("user_id")
         id_Input.send_keys("66")
         id_Input.send_keys(Keys.ENTER)
+        assert 'המצביע אינו מופיע בבסיס הנתונים או שכבר הצביע' not in self.browser.page_source
         #browser.save_screenshot('correctDatails.png')
-        browser.close()
 
     def test_missing_id_(self):
         ################# Try to Get In with incorrect details ##########
-        browser = webdriver.Chrome()
+        browser =  self.browser
         browser.get('http://127.0.0.1:5000/')
         first_name_Input = browser.find_element_by_id("first_name")
         first_name_Input.send_keys("lilo")
@@ -32,53 +34,12 @@ class AppTestCae(unittest.TestCase):
         id_Input = browser.find_element_by_id("user_id")
         id_Input.send_keys("66")
         id_Input.send_keys(Keys.ENTER)
+        assert 'המצביע אינו מופיע בבסיס הנתונים או שכבר הצביע' in self.browser.page_source
         #browser.save_screenshot('incorrectDatails.png')
-        browser.close()
         #################################################################
 
+    def tearDown(self):
+        self.browser.close()
 
 if __name__ == '__main__':
     unittest.main()
-
-
-#################################################################
-
-# from tkinter import Image
-#
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-#
-# ################# Get In with correct details #################
-#
-# chromedriver = "../tests/chromedriver"
-# browser = webdriver.Chrome()
-# browser.get('http://127.0.0.1:5000/')
-# first_name_Input = browser.find_element_by_id("first_name")
-# first_name_Input.send_keys("lilo")
-# last_name_Input = browser.find_element_by_id("last_name")
-# last_name_Input.send_keys("siksik")
-# id_Input =  browser.find_element_by_id("user_id")
-# id_Input.send_keys("66")
-# id_Input.send_keys(Keys.ENTER)
-# browser.save_screenshot('correctDatails.png')
-# browser.close()
-# #################################################################
-#
-# ################# Try to Get In with incorrect details ##########
-#
-# browser = webdriver.Chrome()
-# browser.get('http://127.0.0.1:5000/')
-# first_name_Input = browser.find_element_by_id("first_name")
-# first_name_Input.send_keys("lilo")
-# last_name_Input = browser.find_element_by_id("last_name")
-# last_name_Input.send_keys("horesh")
-# id_Input =  browser.find_element_by_id("user_id")
-# id_Input.send_keys("66")
-# id_Input.send_keys(Keys.ENTER)
-# browser.save_screenshot('incorrectDatails.png')
-# browser.close()
-# #################################################################
-#
-#
-#
-#

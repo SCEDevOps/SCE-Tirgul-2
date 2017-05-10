eb7036fe1fe5ed69e55bb9ad1c47b3465a33a851
LITALHO <litalhoresh@gmail.com>
2017-05-10 11:15:56 +0300

    push tests (try)

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -1,93 +1,84 @@
-from tkinter import Image
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import unittest
+from app import app
 
-################# Get In with correct details #################
+class AppTestCae(unittest.TestCase):
 
-chromedriver = "../tests/chromedriver"
-browser = webdriver.Chrome(chromedriver)
-browser.get('http://127.0.0.1:5000/')
-first_name_Input = browser.find_element_by_id("first_name")
-first_name_Input.send_keys("lilo")
-last_name_Input = browser.find_element_by_id("last_name")
-last_name_Input.send_keys("siksik")
-id_Input =  browser.find_element_by_id("user_id")
-id_Input.send_keys("66")
-id_Input.send_keys(Keys.ENTER)
-browser.save_screenshot('correctDatails.png')
-browser.close()
+
+    def test_Manager_Option(self):
+        #../tests/chromedriver
+        ################# Get In with correct details #################
+        browser = webdriver.Chrome()
+        browser.get('http://127.0.0.1:5000/')
+        first_name_Input = browser.find_element_by_id("first_name")
+        first_name_Input.send_keys("lilo")
+        last_name_Input = browser.find_element_by_id("last_name")
+        last_name_Input.send_keys("siksik")
+        id_Input = browser.find_element_by_id("user_id")
+        id_Input.send_keys("66")
+        id_Input.send_keys(Keys.ENTER)
+        #browser.save_screenshot('correctDatails.png')
+        browser.close()
+
+    def test_missing_id_(self):
+        ################# Try to Get In with incorrect details ##########
+        browser = webdriver.Chrome()
+        browser.get('http://127.0.0.1:5000/')
+        first_name_Input = browser.find_element_by_id("first_name")
+        first_name_Input.send_keys("lilo")
+        last_name_Input = browser.find_element_by_id("last_name")
+        last_name_Input.send_keys("horesh")
+        id_Input = browser.find_element_by_id("user_id")
+        id_Input.send_keys("66")
+        id_Input.send_keys(Keys.ENTER)
+        #browser.save_screenshot('incorrectDatails.png')
+        browser.close()
+        #################################################################
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 #################################################################
 
-################# Try to Get In with incorrect details ##########
-
-browser = webdriver.Chrome(chromedriver)
-browser.get('http://127.0.0.1:5000/')
-first_name_Input = browser.find_element_by_id("first_name")
-first_name_Input.send_keys("lilo")
-last_name_Input = browser.find_element_by_id("last_name")
-last_name_Input.send_keys("horesh")
-id_Input =  browser.find_element_by_id("user_id")
-id_Input.send_keys("66")
-id_Input.send_keys(Keys.ENTER)
-browser.save_screenshot('incorrectDatails.png')
-browser.close()
-#################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# from tkinter import Image
+#
 # from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
-# import unittest
-# from app import app
 #
-# class AppTestCae(unittest.TestCase):
+# ################# Get In with correct details #################
 #
-#
-#     def test_Manager_Option(self):
-#         #../tests/chromedriver
-#         ################# Get In with correct details #################
-#         browser = webdriver.Firefox("../tests/geckodriver")
-#         browser.get('http://127.0.0.1:5000/')
-#         first_name_Input = browser.find_element_by_id("first_name")
-#         first_name_Input.send_keys("lilo")
-#         last_name_Input = browser.find_element_by_id("last_name")
-#         last_name_Input.send_keys("siksik")
-#         id_Input = browser.find_element_by_id("user_id")
-#         id_Input.send_keys("66")
-#         id_Input.send_keys(Keys.ENTER)
-#         browser.save_screenshot('correctDatails.png')
-#         browser.close()
-#
-#     def test_missing_id_(self):
-#         ################# Try to Get In with incorrect details ##########
-#         browser = webdriver.Firefox("../tests/geckodriver")
-#         browser.get('http://127.0.0.1:5000/')
-#         first_name_Input = browser.find_element_by_id("first_name")
-#         first_name_Input.send_keys("lilo")
-#         last_name_Input = browser.find_element_by_id("last_name")
-#         last_name_Input.send_keys("horesh")
-#         id_Input = browser.find_element_by_id("user_id")
-#         id_Input.send_keys("66")
-#         id_Input.send_keys(Keys.ENTER)
-#         browser.save_screenshot('incorrectDatails.png')
-#         browser.close()
-#         #################################################################
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-#
-#
+# chromedriver = "../tests/chromedriver"
+# browser = webdriver.Chrome()
+# browser.get('http://127.0.0.1:5000/')
+# first_name_Input = browser.find_element_by_id("first_name")
+# first_name_Input.send_keys("lilo")
+# last_name_Input = browser.find_element_by_id("last_name")
+# last_name_Input.send_keys("siksik")
+# id_Input =  browser.find_element_by_id("user_id")
+# id_Input.send_keys("66")
+# id_Input.send_keys(Keys.ENTER)
+# browser.save_screenshot('correctDatails.png')
+# browser.close()
 # #################################################################
+#
+# ################# Try to Get In with incorrect details ##########
+#
+# browser = webdriver.Chrome()
+# browser.get('http://127.0.0.1:5000/')
+# first_name_Input = browser.find_element_by_id("first_name")
+# first_name_Input.send_keys("lilo")
+# last_name_Input = browser.find_element_by_id("last_name")
+# last_name_Input.send_keys("horesh")
+# id_Input =  browser.find_element_by_id("user_id")
+# id_Input.send_keys("66")
+# id_Input.send_keys(Keys.ENTER)
+# browser.save_screenshot('incorrectDatails.png')
+# browser.close()
+# #################################################################
+#
+#
+#
+#

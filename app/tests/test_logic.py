27f2f0dd50ca4e2ea90b02122b2cb2dd5c21546d
LITALHO <litalhoresh@gmail.com>
2017-05-18 11:49:14 +0300

    self.get_server_url()

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -20,13 +20,13 @@ class SeleniumTest(unittest.TestCase):
 
     def test_correct_details(self):
         ################# Get In with correct details #################
-        browser =  self.browser
-        browser.get(self.get_server_url())
-        first_name_Input = browser.find_element_by_id("first_name")
+        #browser =  self.browser
+        self.browser.get(self.get_server_url())
+        first_name_Input = self.browser.find_element_by_id("first_name")
         first_name_Input.send_keys("lilo")
-        last_name_Input = browser.find_element_by_id("last_name")
+        last_name_Input = self.browser.find_element_by_id("last_name")
         last_name_Input.send_keys("siksik")
-        id_Input = browser.find_element_by_id("user_id")
+        id_Input = self.browser.find_element_by_id("user_id")
         id_Input.send_keys("66")
         id_Input.send_keys(Keys.ENTER)
         #self.time.sleep(20)

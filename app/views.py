e3a936f1d116d721d7a4ea40b544257dbfba4e29
kbgb666 <kbgb666@gmail.com>
2017-06-02 16:09:19 +0300

    incoding added

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-# - *- coding: utf- 8 - *-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 from flask import render_template, flash, redirect, url_for, request, g
 from flask import send_from_directory

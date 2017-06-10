d9856f17082a5c4e63083f68b87f2372ab6eab63
LITALHO <litalhoresh@gmail.com>
2017-06-10 22:33:09 +0300

    test_full_check

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -73,7 +73,8 @@ class SeleniumTest(LiveServerTestCase):
         id_Input = self.browser.find_element_by_id("user_id")
         id_Input.send_keys("66")
         id_Input.send_keys(Keys.ENTER)
-        self.browser.find_element_by_id("הליכוד").click()
+        likud = self.browser.find_element_by_id("הליכוד")
+        likud.click(self)
         done_btn = self.browser.find_element_by_id("btn")
         done_btn.send_keys(Keys.ENTER)
         Keys.ENTER
